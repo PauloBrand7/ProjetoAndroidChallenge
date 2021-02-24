b43707508eec0da465e64eeb2bf3e66c07af5b8d
Lucas Marciano <lucas.marciano@invillia.com>
2021-02-24 13:51:38 -0300

    fix: inserido pequenos ajustes e comentários

--- app/src/main/java/com/desafio/githubchallenge/ui/activity/MainActivity.kt
+++ app/src/main/java/com/desafio/githubchallenge/ui/activity/MainActivity.kt
@@ -1,53 +1,56 @@
 package com.desafio.githubchallenge.ui.activity
 
-import android.content.res.TypedArray
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.desafio.githubchallenge.R
 import com.desafio.githubchallenge.ui.recyclerview.RepositorioAdapter
 import com.desafio.githubchallenge.ui.viewmodel.MainViewModel
 
+// TODO: a pasta .idea ainda está no seu repo remoto, remova ela
+
 class MainActivity : AppCompatActivity(){
 
     companion object {
-        const val titleAppBar = "GitHub Challenge"  // TODO: usar const -  OK
+        const val titleAppBar = "GitHub Challenge"
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) { //TODO: criar metodos para cada acao feita no onCreate, deixe ele mais limpo - OK
+    // TODO: Atentar ao uso de variáveis globais
+    //TODO: usar sempre a lingua inglesa para nomear os nomes da variáveis, classes e métodos
+
+    private lateinit var viewModel: MainViewModel
+    private val rvList: RecyclerView = findViewById(R.id.activity_cards)
+    private val icones = resources.obtainTypedArray(R.array.iconesNumerados)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cards)
-
-        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        val lista : RecyclerView = findViewById(R.id.activity_cards)
-        val icones  = resources.obtainTypedArray(R.array.iconesNumerados)
-
-        title = titleAppBar
-        liveDataConfigure(viewModel, lista, icones)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        title = titleAppBar // TODO: use uma frase vinda do strings.xml
+        liveDataConfigure()
     }
 
-    private fun liveDataConfigure(viewModel: MainViewModel, lista: RecyclerView, icones: TypedArray) {
-        viewModel.getRepositorios().observe(
-            this,
-            Observer { lista.adapter = RepositorioAdapter(it, icones, ::onClick)} //TODO: usar callback para o click no item - OK
-        )
+    override fun onResume() {
+        super.onResume()
+        viewModel.fetchRepositories()
     }
 
-    fun onClick(message : String) {//TODO: usar os strings.xml, pesquisar como concatenar strings de forma dinamica (string builder) - OK
+    private fun liveDataConfigure() {
+        viewModel.repositoriosLiveData.observe(this, Observer {
+            rvList.apply {
+                // TODO: Passar os icones dessa forma não é o
+                //  jeito certo, tu deve por os icones no seu modelo e
+                //  pegar eles do seu modelo e não de um array do resources
+                adapter = RepositorioAdapter(it, icones, ::onClick)
+            }
+        })
+    }
+
+    // TODO: se ligar na privacidade dos itens que tu implementa
+    private fun onClick(message: String) {
         Toast.makeText(this, StringBuilder().append(resources.getString(R.string.toast_repositorios)).append(message), Toast.LENGTH_SHORT).show()
-        Log.i("toast","Toast Enviado")
     }
-
 }
-
-//TODO: evitar usar o !! - OK
-//TODO: organizar melhor o seu código
-//TODO: procure usar os outros metodos do ciclo de vida
-//TODO: pesquise pelo apply ou o with do koltin, com eles tu pode melhorar muito seu codigo
-//TODO: seu viewModel não faz muito sentido, estude um pouco sobre livedata
-// TODO: procurar sobre o padrão de projeto factory para criar blocos de teste
